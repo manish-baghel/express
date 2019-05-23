3902cef28c8f56f664adeee3b5385b12c085ed51
Manish baghel <baghelmanish660@gmail.com>
2019-05-23 18:04:24 +0530

    mysql config

--- backend/databases/mysql.js
+++ backend/databases/mysql.js
@@ -1,3 +1,35 @@
 // DB configuration goes here
+const Sequelize = require('sequelize');
 
+const sequelize = new Sequelize({
+  database: 'expressBasicLogin',
+  username: 'root',
+  password: 'password',
+  dialect: 'mysql',
+});
+
+
+sequelize
+  .authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch(err => console.error('Unable to connect to the database:', err));
+
+
+// User Model which contains information what entries are there in the 
+// database and which entries are required for a user
+const User = sequelize.define('user',{
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
 
